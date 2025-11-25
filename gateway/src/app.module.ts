efa2425dc808d2fcd99ba2fb3ad566419d1b5a77
Vladlen <vladlen.iliushchenko@gmail.com>
2025-11-26 00:47:30 +0200

    feature(lab-1): start implementing api gateway

--- gateway/src/app.module.ts
+++ gateway/src/app.module.ts
@@ -1,10 +1,23 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ClientsModule, Transport } from '@nestjs/microservices';
+import { join } from 'path';
 
 @Module({
-  imports: [],
+  imports: [
+    ClientsModule.register([
+      {
+        name: "DISPATCHER_PACKAGE",
+        transport: Transport.GRPC,
+        options: {
+          package: 'dispatcher',
+          protoPath: join(__dirname, 'dispatcher/dispatcher.proto'),
+        },
+      }
+    ])
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule { }
--- gateway/src/app.service.ts
+++ gateway/src/app.service.ts
@@ -1,7 +1,13 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, OnModuleInit } from '@nestjs/common';
 
 @Injectable()
-export class AppService {
+export class AppService implements OnModuleInit {
+  private dispatcherService: any;
+
+  onModuleInit() {
+    throw new Error('Method not implemented.');
+  }
+
   createNotification(): string {
     return 'Hello World!';
   }
