c20ae0c47bf83098d80580c7bc7ff8ed1dcb39c2
Vladlen <vladlen.iliushchenko@gmail.com>
2025-11-27 17:59:21 +0200

    refactor(lab-2): start removing sync communication

--- services/dispatcher/src/app.module.ts
+++ services/dispatcher/src/app.module.ts
@@ -1,30 +1,14 @@
 import { Module } from '@nestjs/common';
-import { ConfigModule, ConfigService } from '@nestjs/config';
+import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { ClientsModule, Transport } from '@nestjs/microservices';
-import { join } from 'path';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       isGlobal: true,
       envFilePath: '.env',
-    }),
-    ClientsModule.registerAsync([
-      {
-        name: 'WORKER_PACKAGE',
-        useFactory: (configService: ConfigService) => ({
-          transport: Transport.GRPC,
-          options: {
-            package: configService.get('WORKER_PACKAGE'),
-            protoPath: join(__dirname, '../proto/notification.proto'),
-            url: `${configService.get('WORKER_HOST')}:${configService.get('WORKER_PORT')}`,
-          },
-        }),
-        inject: [ConfigService],
-      },
-    ]),
+    })
   ],
   controllers: [AppController],
   providers: [AppService],
